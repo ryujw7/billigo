4287f46eb4b97d99a7ffc9106853a508b640485e
RYUJI <rgw339@git>
2019-04-06 12:50:47 +0900

    수정2(로그인화면일부)

--- app/src/main/java/com/example/billigo/MainActivity.java
+++ app/src/main/java/com/example/billigo/MainActivity.java
@@ -1,12 +1,38 @@
 package com.example.billigo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends BaseActivity {
-
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new Handler();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                        finish();
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    }
+                });
+
+            }
+        }).start();
     }
 }
