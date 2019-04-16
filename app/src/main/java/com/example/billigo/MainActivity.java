78e9063874a22592cddcf2150a794b20132da961
RYUJI <rgw339@git>
2019-04-17 00:43:11 +0900

    드론요청 등록 액티비티 추가

--- app/src/main/java/com/example/billigo/MainActivity.java
+++ app/src/main/java/com/example/billigo/MainActivity.java
@@ -1,11 +1,27 @@
 package com.example.billigo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.dron_regist).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DronRegistMainActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
+        findViewById(R.id.dron_request).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, DronRequestChaddrActivity.class));
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+        });
     }
 }
