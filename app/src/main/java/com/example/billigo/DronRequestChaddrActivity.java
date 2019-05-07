42f27648a7a707751612fa57576f21515163b48a
RYUJI <rgw339@git>
2019-05-07 23:01:48 +0900

    충동해결

--- app/src/main/java/com/example/billigo/DronRequestChaddrActivity.java
+++ app/src/main/java/com/example/billigo/DronRequestChaddrActivity.java
@@ -7,16 +7,12 @@ public class DronRequestChaddrActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
         setContentView(R.layout.activity_drone_request_changeaddr);
-=======
-        setContentView(R.layout.activity_dron_request_changeaddr);
         findViewById(R.id.adrc_chgregistbtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
->>>>>>> 87046d3f07897b3ac6a9aedb8651ce14780ff6df
     }
 }
